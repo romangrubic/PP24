00f147e5206e79ce88f057479b26cce2ddc429cc
Roman Grubic <grubic.roman@gmail.com>
2021-11-17 18:38:57 +0100

    Rad s Git-om
    Basic rad s Git-om.

---
+++ githubprimjer.sql
@@ -0,0 +1,8 @@
+drop database if exists githubprimjer;
+create database githubprimjer;
+use githubprimjer;
+
+create table git(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null
+);
