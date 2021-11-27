02c8a149394746cdb87869286ee842476af31179
Roman Grubic <grubic.roman@gmail.com>
2021-11-27 20:53:03 +0100

    Vjezba INSERT, UPDATE i JOIN
    Vjezbanje INSERT, UPDATE i JOIN naredbi.

--- SQLskripte/fakultet.sql
+++ SQLskripte/fakultet.sql
@@ -30,3 +30,44 @@ alter table rok add foreign key (kolegij) references kolegij(sifra);
 alter table prijava add foreign key (student) references student(sifra);
 alter table prijava add foreign key (rok) references rok(sifra);
 
+
+-- INSERT
+insert into student(sifra,ime,prezime,oib) values 
+(null,'Roman','Grubić',null),
+(null,'Dunja','Mikić',null),
+(null,'Luka','Tajak',null),
+(null,'Ina','Smiljan',null);
+
+insert into kolegij(sifra,naziv) values 
+(null,'Menadžment'),
+(null,'Poslovna informatika'),
+(null,'Mikroekonomija');
+
+insert into rok(sifra,datum,kolegij) values
+(null,'2021-12-03 12:30',1),
+(null,'2021-12-04 11:30',2),
+(null,'2021-12-05 10:30',3);
+
+insert into prijava(student,rok) values
+(1,2),
+(1,3),
+(2,1),
+(3,2),
+(3,1),
+(4,1),
+(4,3);
+
+-- UPDATE
+update student set oib='56833467683' where sifra=1;
+update kolegij set naziv='Management ljudskih resursa' where sifra=1;
+
+
+-- JOIN
+-- izlistaj sve studente koji su prijavili ispit za 4.prosinca
+
+select a.ime, a.prezime
+from student a
+inner join prijava b on a.sifra=b.student
+inner join rok c on b.rok=c.sifra
+where c.datum like '2021-12-04%';
+
--- SQLskripte/firmapp.sql
+++ SQLskripte/firmapp.sql
@@ -38,3 +38,40 @@ create table transakcija(
 alter table transakcija add foreign key (djelatnik) references djelatnik(id);
 alter table transakcija add foreign key (kupac) references kupac(id);
 alter table transakcija add foreign key (usluga) references usluga(id);
+
+-- INSERT
+insert into djelatnik(id,ime,prezime,oib,email,iban,datumpocetka) values 
+(null,'Roman','Grubić',null,'grubrom@gmail.com',null,'2021-11-23 09:00'),
+(null,'Miki','Milan',null,'mikimilan@gmail.com',null,'2021-11-23 09:00'),
+(null,'Ivanka','Horvat',null,'drmmrm@gmail.com',null,'2021-11-24 09:00');
+
+insert into kupac(id,ime,prezime,oib,email) values 
+(null,'Matej','Klik',null,null),
+(null,'Bojan','Digo',null,null),
+(null,'Darija','Valiant',null,null),
+(null,'Kia','Tomičić',null,null);
+
+insert into usluga(id,naziv,cijena,opis) values
+(null,'Zamjena guma',99.99,null),
+(null,'Pumpanje guma',2.99,null),
+(null,'Balansiranje gume',19.99,null);
+
+insert into transakcija(id,djelatnik,kupac,usluga,vrijemeprodaje) values
+(null,1,3,1,'2021-11-24 13:42:49'),
+(null,1,3,2,'2021-11-24 13:42:49'),
+(null,3,2,3,'2021-11-24 13:49:27'),
+(null,2,1,1,'2021-11-24 13:51:06');
+
+-- UPDATE
+update usluga set cijena=1.99 where naziv='Pumpanje guma';
+update usluga set cijena=14.99 where naziv like '%Balansiranje%';
+
+-- JOIN
+-- izlistaj svo ime i prezime kupaca i djelatnika te vrijeme prodaje 
+-- za sve racune izdane 24. studenog ove godine izmedu 13:40 i 13:50
+
+select a.ime, a.prezime, c.ime, c.prezime, b.vrijemeprodaje
+from kupac a
+inner join transakcija b on b.kupac=a.id
+inner join djelatnik c on b.djelatnik=c.id
+where b.vrijemeprodaje >= '2021-11-24 13:40' and b.vrijemeprodaje <='2021-11-24 13:50'; 
