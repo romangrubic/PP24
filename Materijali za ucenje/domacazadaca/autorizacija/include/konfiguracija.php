99ba1ebe10e5db4f5f877f5d4b8f07ac0529c1b8
Roman Grubic <grubic.roman@gmail.com>
2022-01-12 21:05:55 +0100

    Startanje mini projekta (i dodavanje Foundation CSS)

---
+++ Materijali za ucenje/domacazadaca/autorizacija/include/konfiguracija.php
@@ -0,0 +1,4 @@
+<?php
+
+// deklaracija varijable
+$naslov='Autorizacija';
---
+++ Materijali za ucenje/domacazadaca/autorizacija/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Zaglavlje start -->
+    <?php include_once 'include/zaglavlje.php'; ?>
+    <!-- Zaglavlje kraj -->
+</head>
+
+<body>
+    <div class="callout">
+        <h1>Hello</h1>
+    </div>
+</body>
+
+</html>
