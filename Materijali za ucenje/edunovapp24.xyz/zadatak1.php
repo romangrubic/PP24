fa120d9894fad96762b2fcb28e086122b81021a3
Roman Grubic <grubic.roman@gmail.com>
2022-01-13 20:52:01 +0100

    Rjesavanje zadataka.

--- Materijali za ucenje/edunovapp24.xyz/zadatak1.php
+++ Materijali za ucenje/edunovapp24.xyz/zadatak1.php
@@ -1,3 +1,12 @@
 <?php
 
 // Napišite program koji uneseni broj uvećava za 10 i ispisuje rezultat
+
+if(!isset($_GET['n'])){
+    echo 'Postavite n u adresnu traku';
+    exit;
+}
+
+$n=$_GET['n'];
+
+echo $n+10;
--- Materijali za ucenje/edunovapp24.xyz/zadatak2.php
+++ Materijali za ucenje/edunovapp24.xyz/zadatak2.php
@@ -1,3 +1,24 @@
 <?php
 
 // Napišite program koji za unesena tri broja ispisuje najveći
+
+if(!isset($_GET['broj1'])){
+    echo 'Postavite broj1 u adresnu traku';
+    exit;
+}
+
+if(!isset($_GET['broj2'])){
+    echo 'Postavite broj2 u adresnu traku';
+    exit;
+}
+
+if(!isset($_GET['broj3'])){
+    echo 'Postavite broj3 u adresnu traku';
+    exit;
+}
+
+$broj1=$_GET['broj1'];
+$broj2=$_GET['broj2'];
+$broj3=$_GET['broj3'];
+
+echo max($broj1, $broj2, $broj3);
