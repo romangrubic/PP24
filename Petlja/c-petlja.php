fcd81eec7fc8ba2b9418143806b42f85b6b87b25
Roman Grubic <grubic.roman@gmail.com>
2022-01-17 22:20:09 +0100

    Izvadio sam IF check za row i column iz funkcije spirala na pocetak PHP skripte.

--- Petlja/c-petlja.php
+++ Petlja/c-petlja.php
@@ -22,7 +22,23 @@
 
     <?php
 
-    spirala($_GET['row'], $_GET['column']);
+    // Asking user to set rows in table.
+    if (!isset($_GET['row'])){
+        echo 'Unesite broj redaka u tablici. Hvala. <br/>';
+        exit;
+    }
+    
+    // Asking user to set columns in table.
+    if(!isset($_GET['column'])) {
+        echo 'Unesite broj stupaca u tablici. Hvala. <br/>';
+        exit;
+
+    }
+
+    // If both are set, we can continue and call spirala function with them
+    if(isset($_GET['row']) && isset($_GET['column'])){
+        spirala($_GET['row'], $_GET['column']);
+    }
 
     function spirala($row, $column)
     {
@@ -45,46 +61,45 @@
         // We push the counter in it's position inside multidimensional array
         $list = [];
 
+        // Big loop for filling the list array with correct numbers on correct positions (clockwise)
         while ($counter <= $size) {
             for ($i = $right; $i >= $left; $i--) {
                 $list[$bottom][$i] = $counter++;
             }
             $bottom--;
-            if($counter>$size) break;
+            if ($counter > $size) break;
 
             for ($i = $bottom; $i >= $top; $i--) {
                 $list[$i][$left] = $counter++;
             }
             $left++;
-            if($counter>$size) break;
+            if ($counter > $size) break;
 
             for ($i = $left; $i <= $right; $i++) {
                 $list[$top][$i] = $counter++;
             };
             $top++;
-            if($counter>$size) break;
+            if ($counter > $size) break;
 
             for ($i = $top; $i <= $bottom; $i++) {
                 $list[$i][$right] = $counter++;
             };
             $right--;
-            if($counter>$size) break;
-
+            if ($counter > $size) break;
         };
 
-        if (isset($_GET['row']) && isset($_GET['column'])) {
-            echo '<div class="container mx-auto">';
-            echo '<table>';
-            for ($i = 0; $i <= $row; $i++) {
-                echo '<tr>';
-                for ($j = 0; $j <= $column; $j++) {
-                    echo '<td>' . $list[$i][$j] . '</td>';
-                };
-                echo '</tr>';
+        // Creating table and echoing it on the page.
+        echo '<div class="container mx-auto">';
+        echo '<table>';
+        for ($i = 0; $i <= $row; $i++) {
+            echo '<tr>';
+            for ($j = 0; $j <= $column; $j++) {
+                echo '<td>' . $list[$i][$j] . '</td>';
             };
-            echo '</table>';
-            echo '</div>';
-        }
+            echo '</tr>';
+        };
+        echo '</table>';
+        echo '</div>';
     }
     ?>
 
