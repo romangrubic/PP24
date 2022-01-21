7a7c958fbbdd25100181c6954678d8da246e8d72
Roman Grubic <grubic.roman@gmail.com>
2022-01-21 20:09:35 +0100

    Dodan form element za logiranje korisnika.
    Jednostavan form element za logiranje korisnika pomocu username i password.

--- Materijali za ucenje/domacazadaca/session.projekt/index.php
+++ Materijali za ucenje/domacazadaca/session.projekt/index.php
@@ -3,17 +3,29 @@
 
 <head>
     <!-- Start zaglavlje.php -->
-    <?
+    <?php
     include_once 'includes/zaglavlje.php';
     ?>
     <!-- Zavrsetak zaglavlje.php -->
 </head>
 
 <body>
-    <h1>HRbook</h1>
+    <section>
+        <h1>Dobrodošli na HRbook!</h1>
+        <form action="autoriziraj.php" method="post">
+            <fieldset>
+                <legend>Unesite Vaše detalje za prijavu</legend>
+                <label for="username">Korisničko ime</label>
+                <input type="text" name="username" id="username">
+                <label for="password">Lozinka</label>
+                <input type="password" name="password" id="password">
+                <input type="submit" value="Prijava!">
+            </fieldset>
+        </form>
+    </section>
 
     <!-- Start skripte.php -->
-    <?
+    <?php
     include_once 'includes/skripte.php';
     ?>
     <!-- Zavrsetak skripte.php -->
