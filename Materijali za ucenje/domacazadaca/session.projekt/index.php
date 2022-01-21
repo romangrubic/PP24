e899e2d4482b46de79522372152a7c23bf87457f
Roman Grubic <grubic.roman@gmail.com>
2022-01-21 19:07:32 +0100

    Dodavanje Index.php, CSS-a i JS-a za mini projekt
    Start mini projekta i dodavanje pocetnih file-ova za start.

---
+++ Materijali za ucenje/domacazadaca/session.projekt/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HRbook</title>
+    <link rel="stylesheet" href="css/foundation.css">
+<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <h1>HRbook</h1>
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
