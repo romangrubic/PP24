df5d7da0cc3c01b067d37fa696fcc55912556a76
Roman Grubic <grubic.roman@gmail.com>
2022-01-22 19:32:47 +0100

    Update autoriziraj.php s mini "bazom podataka" u SESSIONu
    Kreiranje mini baze podataka u SESSION objektu tako da se prikazu objave na naslovna.php

--- Materijali za ucenje/domacazadaca/session.projekt/autoriziraj.php
+++ Materijali za ucenje/domacazadaca/session.projekt/autoriziraj.php
@@ -1,15 +1,31 @@
 <?php
 
-if(!isset($_POST['username']) || empty($_POST['username'])){
+if (!isset($_POST['username']) || empty($_POST['username'])) {
     header('location: index.php?error=login');
     exit;
 }
 
-if(!isset($_POST['password']) || empty($_POST['password'])){
+if (!isset($_POST['password']) || empty($_POST['password'])) {
     header('location: index.php?error=login');
     exit;
 }
 
+// Kreiranje SESSION objekta i podataka
 session_start();
-$_SESSION['user']['username']=$_POST['username'];
+$_SESSION['user']['username'] = $_POST['username'];
+$_SESSION['data'] = [
+    [
+        'username' => 'IvanHorvat',
+        'text' => 'Baš me bole leđa. Jel ima tko brufen?'
+    ],
+    [
+        'username' => 'Miki',
+        'text' => 'Dobar dan, jel me netko tražio?'
+    ],
+    [
+        'username' => 'Tekashi69',
+        'text' => 'Igra li itko Tekken?'
+    ]
+];
+// Redirekcija na naslovnu stranicu
 header('location: naslovna.php');
