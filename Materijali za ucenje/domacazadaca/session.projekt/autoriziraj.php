a91bc6ad6fe87ec20dde1202a5e43bf74bd2d942
Roman Grubic <grubic.roman@gmail.com>
2022-01-24 21:22:47 +0100

    Promijenjen uvjet za lozinku te napravio klasu za prijavu Login
    Promijenio sam uvjet za lozinku. Napravio sam klasu Login koja ce, ako je lozinka tocna, kreirati SESSION objekt sa podacima.

--- Materijali za ucenje/domacazadaca/session.projekt/autoriziraj.php
+++ Materijali za ucenje/domacazadaca/session.projekt/autoriziraj.php
@@ -1,31 +1,17 @@
 <?php
-
 if (!isset($_POST['username']) || empty($_POST['username'])) {
     header('location: index.php?error=login');
     exit;
 }
 
-if (!isset($_POST['password']) || empty($_POST['password'])) {
+if (!isset($_POST['password']) || $_POST['password'] !== 'lozinka') {
     header('location: index.php?error=login');
     exit;
 }
 
-// Kreiranje SESSION objekta i podataka
+require_once 'classes/Funkcije.classes.php';
+
+// Zvanje klase za kreiranje SESSIONa i redirekcija na naslovna.php
 session_start();
-$_SESSION['user']['username'] = $_POST['username'];
-$_SESSION['data'] = [
-    [
-        'username' => 'IvanHorvat',
-        'text' => 'Baš me bole leđa. Jel ima tko brufen?'
-    ],
-    [
-        'username' => 'Miki',
-        'text' => 'Dobar dan, jel me netko tražio?'
-    ],
-    [
-        'username' => 'Tekashi69',
-        'text' => 'Igra li itko Tekken?'
-    ]
-];
-// Redirekcija na naslovnu stranicu
+Login::setLogin($_POST['username']);
 header('location: naslovna.php');
--- Materijali za ucenje/domacazadaca/session.projekt/classes/Funkcije.classes.php
+++ Materijali za ucenje/domacazadaca/session.projekt/classes/Funkcije.classes.php
@@ -1,12 +1,37 @@
 <?php
 
-class Post 
+class Login
 {
-    public static function writePost($post){
+    // Stvara SESSION detalje pri logiranju
+    public static function setLogin($username)
+    {
+        $_SESSION['user']['username'] = $username;
+        $_SESSION['data'] = [
+            [
+                'username' => 'IvanHorvat',
+                'text' => 'Baš me bole leđa. Jel ima tko brufen?'
+            ],
+            [
+                'username' => 'Miki',
+                'text' => 'Dobar dan, jel me netko tražio?'
+            ],
+            [
+                'username' => 'Tekashi69',
+                'text' => 'Igra li itko Tekken?'
+            ]
+        ];
+    }
+}
+
+class Post
+{
+    // Ispisuje sve objave u SESSION DATA
+    public static function writePost($post)
+    {
         echo '
             <div>
-                <p>'.$post['text'].'</p>
-                <h4>'.$post['username'].'</h4>
+                <p>' . $post['text'] . '</p>
+                <h4>' . $post['username'] . '</h4>
             </div>
             <hr />
         ';
